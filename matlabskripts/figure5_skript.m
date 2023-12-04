13a2d6972dd6b1c4f9ffcbdde2d3389f8c1c3dbd
JRS <123469125+mrmeeseeksjr@users.noreply.github.com>
2023-12-04 12:00:23 +0100

    Add files via upload

--- matlabskripts/figure5_skript.m
+++ matlabskripts/figure5_skript.m
@@ -1,36 +1,34 @@
 %% Figure 5
 % Simulation opf Pulse Shapes
 
-% Go to the example folder
-
-M_z1 = simulate_pulseqcest('Sinc_B1_1p80_DC90_td.seq','CSF_20mM.yaml');
-M_z2 = simulate_pulseqcest('Gauss_B1_1p80_DC90_td.seq','CSF_20mM.yaml');
-M_z3 = simulate_pulseqcest('Block_B1_1p80_DC90_td.seq','CSF_20mM.yaml');
-M_z4 = simulate_pulseqcest('Sinc_B1_1p80_DC90_spoiler.seq','CSF_20mM.yaml');
-M_z5 = simulate_pulseqcest('Gauss_B1_1p80_DC90_spoiler.seq','CSF_20mM.yaml');
-M_z6 = simulate_pulseqcest('Block_B1_1p80_DC90_spoiler.seq','CSF_20mM.yaml');
-M_z7 = simulate_pulseqcest('Sinc_B1_1p80_DC90_spoiler.seq','CSF_20mM_dB0.yaml');
-M_z8 = simulate_pulseqcest('Gauss_B1_1p80_DC90_spoiler.seq','CSF_20mM_dB0.yaml');
-M_z9 = simulate_pulseqcest('Block_B1_1p80_DC90_spoiler.seq','CSF_20mM_dB0.yaml');
-
-
-%Plot
+M1 = simulate_pulseqcest('Sinc_delay.seq','WM_3T_20mM_L-arginin.yaml');
+M2 = simulate_pulseqcest('Gauss_delay.seq','WM_3T_20mM_L-arginin.yaml');
+M3 = simulate_pulseqcest('Block_delay.seq','WM_3T_20mM_L-arginin.yaml');
+M4 = simulate_pulseqcest('Sinc_spoiler.seq','WM_3T_20mM_L-arginin.yaml');
+M5 = simulate_pulseqcest('Gauss_spoiler.seq','WM_3T_20mM_L-arginin.yaml');
+M6 = simulate_pulseqcest('Block_spoiler.seq','WM_3T_20mM_L-arginin.yaml');
+M7 = simulate_pulseqcest('Sinc_spoiler.seq','WM_3T_20mM_L-arginin_B0Drift.yaml');
+M8 = simulate_pulseqcest('Gauss_spoiler.seq','WM_3T_20mM_L-arginin_B0Drift.yaml');
+M9 = simulate_pulseqcest('Block_spoiler.seq','WM_3T_20mM_L-arginin_B0Drift.yaml');
 fig=figure
 subplot(3,3,1)
-plot(-4:0.05:4,abs(M_z1(2:end)));title('Sinc');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
+plot(-4:0.05:4,abs(M1(2:end)));title('Sinc pulse shape');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
 subplot(3,3,2)
-plot(-4:0.05:4,abs(M_z2(2:end)));title('Gauss');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
+plot(-4:0.05:4,abs(M2(2:end)));title('Gauss pulse shape');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
 subplot(3,3,3)
-plot(-4:0.05:4,abs(M_z3(2:end)));title('Block');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
+plot(-4:0.05:4,abs(M3(2:end)));title('Block pulse shape');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
 subplot(3,3,4)
-plot(-4:0.05:4,abs(M_z4(2:end)));title('Sinc with Spoiler during Td');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
+plot(-4:0.05:4,abs(M4(2:end)));title('Sinc pulse shape with gradient spoiling');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
 subplot(3,3,5)
-plot(-4:0.05:4,abs(M_z5(2:end)));title('Gauss with Spoiler during Td');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
+plot(-4:0.05:4,abs(M5(2:end)));title('Gauss pulse shape with gradient spoiling');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
 subplot(3,3,6)
-plot(-4:0.05:4,abs(M_z6(2:end)));title('Block with Spoiler during Td');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
+plot(-4:0.05:4,abs(M6(2:end)));title('Block pulse shape with gradient spoiling');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
 subplot(3,3,7)
-plot(-4:0.05:4,abs(M_z7(2:end)));title('Sinc with Spoiler during Td');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
+plot(-4:0.05:4,abs(M7(2:end)));title('Sinc pulse shape with gradient spoiling +B0 Drift');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
 subplot(3,3,8)
-plot(-4:0.05:4,abs(M_z8(2:end)));title('Gauss with Spoiler during Td');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
+plot(-4:0.05:4,abs(M8(2:end)));title('Gauss pulse shape with gradient spoiling +B0 Drift');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
 subplot(3,3,9)
-plot(-4:0.05:4,abs(M_z9(2:end)));title('Block with Spoiler during Td');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
+plot(-4:0.05:4,abs(M9(2:end)));title('Block pulse shape with gradient spoiling +B0 Drift');ylabel('Z(\Delta\omega)');xlabel('\Delta\omega [ppm]');ylim([0 1]);xlim([-4 4]);set(gca, 'XDir','reverse')
+
+
+
